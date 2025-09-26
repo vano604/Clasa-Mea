590ca3562aaaceb29c8518c870040353eea61944
vano604 <vlassivvan@gmail.com>
2025-09-26 17:31:02 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,6 @@
+var data = new Date();
+document.getElementById('an').innerHTML = data.getFullYear();
+
+function mobileMenu() {
+    document.getElementById("navbar").classList.toggle("mobile");
+  }
